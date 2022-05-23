18c4d115cd3198b7759e131b593399a5eb59d8fc
Raheel1258 <rahilahmad1258@gmail.com>
2022-05-23 16:18:55 +0500

    - Add code for push notification - Xcode bundle identifier exist

--- App.js
+++ App.js
@@ -6,8 +6,51 @@ import {Provider} from 'react-redux';
 import store from "./src/store/store"
 import Toast from 'react-native-toast-message';
 import SplashScreen from 'react-native-splash-screen';
+import OneSignal from 'react-native-onesignal';
 
 import Routes from './src/Utils/Routes';
+
+const OneSignalComponent = () => {
+  //OneSignal Init Code
+  OneSignal.setLogLevel(6, 0);
+  //Android id ---- ? -----
+  //Ios ID ---- 041fb0c4-d5be-4a35-bcde-68e3be50d503 ----
+  OneSignal.setAppId('041fb0c4-d5be-4a35-bcde-68e3be50d503');
+  //END OneSignal Init Code
+
+  //Prompt for push on iOS
+  OneSignal.promptForPushNotificationsWithUserResponse(response => {
+    console.log('Prompt response:', response);
+  });
+
+  //Method for handling notifications received while app in foreground
+  OneSignal.setNotificationWillShowInForegroundHandler(
+    notificationReceivedEvent => {
+      console.log(
+        'OneSignal: notification will show in foreground:',
+        notificationReceivedEvent,
+      );
+      let notification = notificationReceivedEvent.getNotification();
+      console.log('notification: ', notification);
+      const data = notification.additionalData;
+      console.log('additionalData: ', data);
+      // Complete with null means don't show a notification.
+      notificationReceivedEvent.complete(notification);
+    },
+  );
+
+  //Method for handling notifications opened
+  OneSignal.setNotificationOpenedHandler(notification => {
+    console.log(
+      'OneSignal: notification opened:',
+      notification.notification?.additionalData?.route,
+    );
+    // Linking.openURL(
+    //   `demo://${notification.notification?.additionalData?.route}`,
+    // );
+  });
+  return <></>;
+};
 
 const App = () => {
 
@@ -24,6 +67,7 @@ const App = () => {
           <Routes />
         </NavigationContainer>
         <Toast/>
+        <OneSignalComponent/>
     </SafeAreaView>
     </Provider>
   );

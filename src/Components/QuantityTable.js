4eb6d2de2817d422dc0963155d91ae65d9e61d7f
Raheel1258 <rahilahmad1258@gmail.com>
2022-07-20 14:59:04 +0500

    changing price chart

--- src/Components/QuantityTable.js
+++ src/Components/QuantityTable.js
@@ -16,7 +16,7 @@ const QuantityTable = ({selectedPriceChart, setSelectedPriceChart, priceChartAni
       <TouchableOpacity activeOpacity={1} onPress = { () => setSelectedPriceChart(item)} style={{...styles.tableItems, ...quantityStyle}}>
         <Text style={styles.priceText}>{item?.units}</Text>
         <Text style={styles.dollerPrice}>${Math.round(Number(item?.units * item?. pricePerUnit))}</Text>
-        <Text style={styles.priceText}>${Math.round(Number(item?.pricePerUnit))}</Text>
+        <Text style={styles.priceText}>${(Number(item?.pricePerUnit))?.toFixed(2)}</Text>
       </TouchableOpacity>
     );
   };

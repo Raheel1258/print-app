dfd8987c3e579c0b24a553f83d5b9bee627b540e
Raheel1258 <rahilahmad1258@gmail.com>
2022-10-11 15:28:52 +0500

    changing into status bar for android

--- src/Components/StatusBarComponent.js
+++ src/Components/StatusBarComponent.js
@@ -6,7 +6,7 @@ const StatusBarComponent = ({ padding = 0, color = null, extraPadding = 0 }) => 
   console.log(Appearance.getColorScheme());
   return (
     <View style={{ marginTop: 0, backgroundColor: '#fff' }}>
-      <View style={{ marginTop: Platform.OS === "ios" ? 50 : 0 }} />
+      <View style={{ marginTop: Platform.OS === "ios" ? 50 : 10 }} />
       <StatusBar backgroundColor={"white"} barStyle={'dark-content'} />
     </View>
   );
